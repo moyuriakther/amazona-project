078c84d355bd63cf02599bba9eb1d0fba379f0ee
Moyuri Akther <akthermoyuri@.com>
2022-04-26 01:45:31 +0600

    share code on github

---
+++ amazon-frontend/src/App.js
@@ -0,0 +1,47 @@
+import data from './data.js';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">Amazon</a>
+        </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <div className="row center">
+          {
+            data.products.map(product =>(
+              <div key={product._id} className="card">
+              <a href={`/product/${product._id}`}>
+                {/* image size 680px by 830px */}
+                <img className="medium" src={product.image} alt={product.name} />
+              </a>
+              <div className="card-body">
+                <a href={`/product/${product._id}`}><h2>{product.name}</h2></a>
+                <div className="rating">
+                  <span><i className="fa fa-star"></i></span>
+                  <span><i className="fa fa-star"></i></span>
+                  <span><i className="fa fa-star"></i></span>
+                  <span><i className="fa fa-star"></i></span>
+                  <span><i className="fa fa-star"></i></span>
+                </div>
+                <div className="price"><h2>${product.price}</h2></div>
+              </div>
+            </div>
+            ))
+          }          
+        </div>
+      </main>
+      <footer className="row center">All right reserved</footer>
+    </div>  
+  </div>
+  );
+}
+
+export default App;
---
+++ amazon-frontend/src/data.js
@@ -0,0 +1,71 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            price: 120,
+            rating: 4.5,
+            numReviews: 10,
+            category: 'Shirts',
+            brand: 'Nike',
+            description: 'high quality product',
+            image: 'https://5.imimg.com/data5/UT/LP/MY-43691381/blue-button-up-shirt-mens-black-and-gold-shirt-mens-india-500x500.jpg'
+        },
+        {
+            _id: '2',
+            name: 'Adidas Fit Shirt',
+            price: 180,
+            rating: 4.4,
+            numReviews: 15,
+            category: 'Shirts',
+            brand: 'Adidas',
+            description: 'high quality product',
+            image: 'https://5.imimg.com/data5/ANDROID/Default/2020/10/DA/GX/TA/115146420/product-jpeg-500x500.jpg'
+        },
+        {
+            _id: '3',
+            name: 'Nike Slim Pant',
+            price: 140,
+            rating: 4.9,
+            numReviews: 11,
+            category: 'Pants',
+            brand: 'Nike',
+            description: 'high quality product',
+            image: 'https://i.pinimg.com/236x/0f/9c/9f/0f9c9f7c5f669b5d9b45dc8cd26d6c2f.jpg'
+        },
+        {
+            _id: '4',
+            name: 'Nike Bright Shirt',
+            price: 220,
+            rating: 4.2,
+            numReviews: 8,
+            category: 'Shirts',
+            brand: 'Nike',
+            description: 'high quality product',
+            image: 'https://assets.ajio.com/medias/sys_master/root/20210420/R70k/607ecc5baeb269a9e3972075/dennislingo_premium_attire_green_full_sleeves_slim_fit_shirt.jpg'
+        },
+        {
+            _id: '5',
+            name: 'Nike Slim Shirt',
+            price: 120,
+            rating: 4.5,
+            numReviews: 10,
+            category: 'Shirts',
+            brand: 'Nike',
+            description: 'high quality product',
+            image: 'https://m.media-amazon.com/images/I/611lq9MXqQL._AC_UX522_.jpg'
+        },
+        {
+            _id: '6',
+            name: 'Nike White Shirt',
+            price: 160,
+            rating: 4.7,
+            numReviews: 13,
+            category: 'Shirts',
+            brand: 'Nike',
+            description: 'high quality product',
+            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS40W-s7Rx0pCL5Yp8AAc2FqLoYBG3bWV77NQ&usqp=CAU'
+        }
+    ]
+}
+export default data;
